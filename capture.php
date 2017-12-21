e37b6318ca7b36d5a9bab8648b6ff3f74422c205
Kim Ortvald <keiko@keikoware.dk>
2017-12-22 00:34:09 +0100

    Update capture.php

--- capture.php
+++ capture.php
@@ -1,10 +1,22 @@
 <?
 include("../dbconnect/dbconnect.php");
+    function getRealIpAddr(){
+        if(!empty($_SERVER['HTTP_CLIENT_IP'])){  //check ip from share internet
+            $ip=$_SERVER['HTTP_CLIENT_IP'];
+        } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){   //to check ip is pass from proxy
+            $ip=$_SERVER['HTTP_X_FORWARDED_FOR'];
+        } else {
+            $ip=$_SERVER['REMOTE_ADDR'];
+        }
+        return $ip;
+    }
 $s = @$_REQUEST["sensor"];
+$uid = @$_REQUEST["uid"];
 $t = @$_REQUEST["temperature"];
 $h = @$_REQUEST["humidity"];
-$sql = "INSERT INTO temperature (sensor,temperature,humidity) VALUES('$s','$t','$h');";
+$ip = getRealIpAddr();
+$sql = "INSERT INTO temperature (sensor,temperature,humidity,RPIuniqueId,IPadresses) VALUES('$s','$t','$h','$uid','$ip');";
 mysql_query($sql);
 mysql_close();
-echo "OK [$s,$t,$h] " . mysql_error();
+echo "OK " . mysql_error();
 ?> 
